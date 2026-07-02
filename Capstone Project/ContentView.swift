9fa217fed2e6478c4148e80be9a35aceba02a011
Nathanael Pobee <nnpobeee@gmail.com>
2026-07-02 12:15:56 -0500

    Update ContentView.swift

--- Capstone Project/ContentView.swift
+++ Capstone Project/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Color.purple
+            Circle()
+            Color.green
+        
         }
         .padding()
     }
