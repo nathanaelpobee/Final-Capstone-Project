2601d58a6a7f890f835074db27f973ea3958365d
Nathanael Pobee <nnpobeee@gmail.com>
2026-07-02 11:18:41 -0500

    Initial Commit

---
+++ Capstone Project/Capstone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_ProjectApp.swift
+//  Capstone Project
+//
+//  Created by DPI Student 013 on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capstone Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capstone Project
+//
+//  Created by DPI Student 013 on 7/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
